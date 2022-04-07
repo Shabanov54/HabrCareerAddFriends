0b41a1daeaae76fc1c5f081ae0c13dd642fc1eef
Станислав Шабанов <marazm56@gmail.com>
2022-04-07 20:11:31 +0700

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace HabrCareerAddFriends
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LoginTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lable2 = new System.Windows.Forms.Label();
+            this.PassTextBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.URLtextBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.StartPageTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LoginTextBox
+            // 
+            this.LoginTextBox.Location = new System.Drawing.Point(36, 28);
+            this.LoginTextBox.Name = "LoginTextBox";
+            this.LoginTextBox.Size = new System.Drawing.Size(215, 20);
+            this.LoginTextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Логин";
+            // 
+            // lable2
+            // 
+            this.lable2.AutoSize = true;
+            this.lable2.Location = new System.Drawing.Point(36, 68);
+            this.lable2.Name = "lable2";
+            this.lable2.Size = new System.Drawing.Size(45, 13);
+            this.lable2.TabIndex = 3;
+            this.lable2.Text = "Пароль";
+            // 
+            // PassTextBox2
+            // 
+            this.PassTextBox2.Location = new System.Drawing.Point(36, 87);
+            this.PassTextBox2.Name = "PassTextBox2";
+            this.PassTextBox2.Size = new System.Drawing.Size(215, 20);
+            this.PassTextBox2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(146, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Адрес профиля с друзьями";
+            // 
+            // URLtextBox3
+            // 
+            this.URLtextBox3.Location = new System.Drawing.Point(36, 145);
+            this.URLtextBox3.Name = "URLtextBox3";
+            this.URLtextBox3.Size = new System.Drawing.Size(538, 20);
+            this.URLtextBox3.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(39, 246);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // StartPageTextBox
+            // 
+            this.StartPageTextBox.Location = new System.Drawing.Point(39, 200);
+            this.StartPageTextBox.Name = "StartPageTextBox";
+            this.StartPageTextBox.Size = new System.Drawing.Size(100, 20);
+            this.StartPageTextBox.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 184);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(227, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Начальная страница (Можно не заполнять)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(581, 290);
+            this.Controls.Add(this.StartPageTextBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.URLtextBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lable2);
+            this.Controls.Add(this.PassTextBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LoginTextBox);
+            this.Name = "Form1";
+            this.Text = "Habr Career";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox LoginTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lable2;
+        private System.Windows.Forms.TextBox PassTextBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox URLtextBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox StartPageTextBox;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,126 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HabrCareerAddFriends
+{
+    public partial class Form1 : Form
+    {
+        int page;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private  void button1_Click(object sender, EventArgs e)
+        {
+            if (StartPageTextBox.Text == "")
+            {
+                page = 1;
+            }
+            else
+            {
+                page = Convert.ToInt32(StartPageTextBox.Text);
+            }
+
+            if (LoginTextBox.Text != "" && PassTextBox2.Text != "" && URLtextBox3.Text != "")
+            {
+                if (LoginTextBox.Text != "" && PassTextBox2.Text != "")
+                {
+                    Properties.Settings.Default.login = LoginTextBox.Text;
+                    Properties.Settings.Default.pass = PassTextBox2.Text;
+                    Properties.Settings.Default.Save();
+
+                }
+                IWebDriver driver = new ChromeDriver(); ;
+                //driver.Url = @"https://account.habr.com/login/?state=2b560e9ae7302456aa935e9f7f9bc042&consumer=habr&hl=ru_RU";
+                driver.Url = "https://career.habr.com/";
+                Thread.Sleep(2000);
+                //await Task.Delay(2000);
+                driver.FindElement(By.PartialLinkText("Войти")).Click();
+                //await Task.Delay(2000);
+                Thread.Sleep(2000);
+
+                driver.FindElement(By.XPath(@"//input[contains(@name,'email')]")).SendKeys(LoginTextBox.Text.Trim());
+                driver.FindElement(By.XPath(@"//input[contains(@type,'password')]")).SendKeys(PassTextBox2.Text.Trim());
+                driver.FindElement(By.XPath(@"//button[contains(@type,'submit')]")).Click();
+                //await Task.Delay(5000);
+                Thread.Sleep(5000);
+
+                //https://career.habr.com/elle-shabli7/friends
+                //driver.Url = URLtextBox3.Text.Trim();
+                //await Task.Delay(2000);
+                Thread.Sleep(2000);
+                bool reload = false;
+                bool select = true;
+                while (select==true)
+                {
+                    reload = false;
+                    string x ="";
+                    driver.Url = $"{URLtextBox3.Text}?page={page}";
+
+                    Thread.Sleep(3000);
+                    int count = 0;
+                    IList<IWebElement> addFriends = driver.FindElements(By.ClassName("dropdown-target"));
+                    for (int i = 0; i < addFriends.Count; i++)
+                    {
+                        if (count==3)
+                        {
+                            driver.Url = $"{URLtextBox3.Text}?page={page}";
+                            reload = true;
+                            break;
+                        }
+                        if (addFriends[i].Text == "В друзья")
+                        {
+                            try
+                            {
+                                addFriends[i].Click();
+                                //await Task.Delay(12000);
+                                Thread.Sleep(12000);
+
+                            }
+                            catch (Exception ex)
+                            {
+                                count++;
+                            }
+                        }
+                    }
+                    if (select==false)
+                    {
+                        break;
+                    }
+                    //if (reload == false)
+                    //{
+                    //    page++;
+                    //    Thread.Sleep(2000);
+                    //}
+                    page++;
+                    Thread.Sleep(2000);
+
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("Не все поля заполнены");
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (Properties.Settings.Default.login.ToString() != "")
+            {
+                LoginTextBox.Text = Properties.Settings.Default.login;
+                PassTextBox2.Text = Properties.Settings.Default.pass;
+            }
+        }
+    }
+}
